65aac6b3a07db2fcd140af29b5cbaa2413a50203
yangwoohyeon <dngus0717@gmail.com>
2025-05-25 14:02:03 +0900

    first commit

---
+++ src/main/java/com/example/github_actions_docker/AppController.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_docker;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String index(){
+        return "hello";
+    }
+}
---
+++ src/main/java/com/example/github_actions_docker/Application.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
