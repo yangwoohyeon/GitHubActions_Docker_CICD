da2893aa37d975eab60c6c62dcc0cb8c206cfb74
yangwoohyeon <dngus0717@gmail.com>
2025-05-25 16:26:47 +0900

    트리거 테스트

--- src/main/java/com/example/github_actions_docker/AppController.java
+++ src/main/java/com/example/github_actions_docker/AppController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String index(){
-        return "nice";
+        return "hello!!";
     }
 }
